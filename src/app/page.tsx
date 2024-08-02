6eebe5da2b564fcd8fa55ee384ce24a81f4278ef
youxiang-git <chaiyouxiang@gmail.com>
2024-08-02 17:50:57 +0800

    finished layouts chapter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,51 @@
 
 export default function Home() {
   return (
-    // Grid cols
+    // Containers
     // <div className="h-screen text-white Parent bg-slate-900">
-    //   <div className="container mx-auto">
-    //     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-    //       <div className="bg-sky-500 p-6 rounded-lg col-start-2">First column</div>
-    //       <div className="bg-sky-500 p-6 rounded-lg col-start-1">Second column</div>
-    //       <div className="bg-red-500 p-6 rounded-lg col-span-2">Third column</div>
-    //       <div className="bg-sky-500 p-6 rounded-lg ">Fourth column</div>
-    //       <div className="bg-sky-500 p-6 rounded-lg col-start-3">Fifth column</div>
-    //       <div className="bg-red-500 p-6 rounded-lg col-span-2">Sixth column</div>
-    //     </div>
+    //   <div className="container px-2 columns-3">
+    //     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit obcaecati debitis facilis quae adipisci iste? Laboriosam quae magnam molestias nesciunt minima, exercitationem, doloribus veritatis perferendis quod corporis deserunt, illum ullam?</p>
+    //     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit obcaecati debitis facilis quae adipisci iste? Laboriosam quae magnam molestias nesciunt minima, exercitationem, doloribus veritatis perferendis quod corporis deserunt, illum ullam?</p>
+    //     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit obcaecati debitis facilis quae adipisci iste? Laboriosam quae magnam molestias nesciunt minima, exercitationem, doloribus veritatis perferendis quod corporis deserunt, illum ullam?</p>
     //   </div>
     // </div>
-    
-    // Grid rows
-    // <div className="h-screen text-white Parent bg-slate-900">
-    //   <div className="container mx-auto">
-    //     <div className="grid grid-flow-col grid-rows-4 gap-4">
-    //       <div className="row-span-3 p-6 rounded-lg bg-sky-500">01</div>
-    //       <div className="p-6 rounded-lg bg-sky-500">02</div>
-    //       <div className="p-6 rounded-lg bg-sky-500">03</div>
-    //       <div className="p-6 rounded-lg bg-sky-500">04</div>
-    //       <div className="row-span-2 p-6 rounded-lg bg-sky-500">05</div>
-    //       <div className="p-6 rounded-lg bg-sky-500">06</div>
-    //       <div className="p-6 rounded-lg bg-sky-500">07</div>
-    //       <div className="p-6 rounded-lg bg-sky-500">08</div>
-    //       <div className="p-6 rounded-lg bg-sky-500">09</div>
-    //       <div className="p-6 rounded-lg bg-sky-500">10</div>
-    //       <div className="p-6 rounded-lg bg-sky-500">11</div>
-    //       <div className="p-6 rounded-lg bg-sky-500">12</div>
+
+    // Float & Clear
+    //   <div className="h-screen text-white Parent bg-slate-900">
+    //   <div className="container px-2">
+    //     <img src="https://www.tangledupinfood.com/sites/default/files/monaco_new_harbor.jpg" className="w-64 float-left"/>
+    //     <img src="https://www.tangledupinfood.com/sites/default/files/monaco_new_harbor.jpg" className="w-32 float-right"/>
+    //     <p className="clear-none">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit obcaecati debitis facilis quae adipisci iste? Laboriosam quae magnam molestias nesciunt minima, exercitationem, doloribus veritatis perferendis quod corporis deserunt, illum ullam?</p>
+    //     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit obcaecati debitis facilis quae adipisci iste? Laboriosam quae magnam molestias nesciunt minima, exercitationem, doloribus veritatis perferendis quod corporis deserunt, illum ullam?</p>
+    //     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit obcaecati debitis facilis quae adipisci iste? Laboriosam quae magnam molestias nesciunt minima, exercitationem, doloribus veritatis perferendis quod corporis deserunt, illum ullam?</p>
+    //   </div>
+    // </div>
+
+    // Position
+    // <div className="text-white Parent bg-slate-900">
+    //   <div className="container px-2">
+    //     <div className="relative w-32 h-32 bg-red-500 rounded-lg p-8">
+    //       <div className="absolute inset-2 w-24 h-24 bg-green-500 rounded-lg"></div>
     //     </div>
     //   </div>
     // </div>
 
-    // Grid Auto flow - automatically fills empty space
-    <div className="h-screen text-white Parent bg-slate-900">
-    <div className="container mx-auto">
-      <div className="grid grid-flow-row-dense grid-cols-3 grid-rows-3 gap-4">
-        <div className="col-span-2 p-6 rounded-lg bg-sky-500">01</div>
-        <div className="col-span-2 p-6 rounded-lg bg-sky-500">02</div>
-        <div className="p-6 rounded-lg bg-sky-500">03</div>
-        <div className="p-6 rounded-lg bg-sky-500">04</div>
-        <div className="p-6 rounded-lg bg-sky-500">05</div>
-        <div className="col-span-2 p-6 rounded-lg bg-sky-500">06</div>
-        <div className="p-6 rounded-lg bg-sky-500">07</div>
-        <div className="p-6 rounded-lg bg-sky-500">08</div>
-        <div className="p-6 rounded-lg bg-sky-500">09</div>
-        <div className="col-span-2 p-6 rounded-lg bg-sky-500">10</div>
-        <div className="p-6 rounded-lg bg-sky-500">11</div>
-        <div className="p-6 rounded-lg bg-sky-500">12</div>
+    // Z-order/position
+    // <div className="text-white Parent bg-slate-900 Parent">
+    //   <div className="container flex">
+    //     <div className="bg-red-500 -mr-2 border p-3 border-white rounded-full z-40">05</div>
+    //     <div className="bg-red-500 -mr-2 border p-3 border-white rounded-full z-30">04</div>
+    //     <div className="bg-red-500 -mr-2 border p-3 border-white rounded-full z-20">03</div>
+    //     <div className="bg-red-500 -mr-2 border p-3 border-white rounded-full z-10">02</div>
+    //     <div className="bg-red-500 -mr-2 border p-3 border-white rounded-full z-0">01</div>
+    //   </div>
+    // </div>
+
+    // Object position
+    <div className="text-white Parent bg-slate-900 Parent">
+      <div className="container">
+        <img className="object-none object-right-bottom w-64 h-64" src="https://www.tangledupinfood.com/sites/default/files/monaco_new_harbor.jpg" alt=""/>
       </div>
     </div>
-  </div>
   );
 }
