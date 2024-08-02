7a308ed5a9d48bb33c8682ee8ce481ce45dd97b0
youxiang-git <chaiyouxiang@gmail.com>
2024-08-02 18:40:45 +0800

    finished effects \& filters chapter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,23 @@
 
 export default function Home() {
   return (
-    // Borders
-    // <div className="h-screen text-white bg-slate-900 Parent">
-    //   <header className="w-full py-4 border-b-2 border-l-8 border-t-8">Header</header>
-    // </div>
-
-    // Divides (border)
-    // <div className="h-screen text-white bg-slate-900 Parent">
-    //   <div className="grid grid-cols-4 divide-x divide-red-500 divide-dotted">
-    //     <div>01</div>
-    //     <div>02</div>
-    //     <div>03</div>
-    //     <div>04</div>
+    // Box shadows
+    // <div className="h-screen p-4 Parent w-64">
+    //   <div className="incline-block p-4 text-black bg-white border rounded-lg shadow-lg shadow-cyan-700">
+    //     <h1 className="text-2xl">Hello</h1>
+    //     <p className="mb-4">This is my div</p>
+    //     <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 opacity-20">Say hello</button>
     //   </div>
     // </div>
 
-    // Border and border styles
-    //   <div className="h-screen text-white bg-slate-900 Parent">
-    //   <input type="text" className="border-2 border-dotted border-rose-600 outline-none"/>
-    // </div>
-
-    // Outlines
-    // <div className="h-screen text-white bg-slate-900 Parent">
-    //   <button className="ml-2 mt-2 outline outline-offset-2 outline-4">Button C</button>
-    // </div>
-
-    // Rings
-    <div className="h-screen text-white bg-slate-900 Parent">
-      <button className="ml-2 mt-2 ring-offset-2 ring-4">Button C</button>
+    // Filters
+    <div className="h-screen p-4 Parent w-64">
+      <div className="incline-block p-4 text-black bg-white border rounded-lg shadow-md grayscale brightness-50 hover:brightness-100">
+        <img src="https://www.tangledupinfood.com/sites/default/files/monaco_new_harbor.jpg" alt="" className="w-64 mb-4"/>
+        <h1 className="text-2xl">Hello</h1>
+        <p className="mb-4">This is my div</p>
+        <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 opacity-20">Say hello</button>
+      </div>
     </div>
   );
 }
