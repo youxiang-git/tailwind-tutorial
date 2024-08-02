db3d129d9dbb2bff62a06009145f0404b2b43ce4
youxiang-git <chaiyouxiang@gmail.com>
2024-08-02 19:20:34 +0800

    finished animations chapter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
 
 export default function Home() {
   return (
-    // Box shadows
-    // <div className="h-screen p-4 Parent w-64">
-    //   <div className="incline-block p-4 text-black bg-white border rounded-lg shadow-lg shadow-cyan-700">
-    //     <h1 className="text-2xl">Hello</h1>
-    //     <p className="mb-4">This is my div</p>
-    //     <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 opacity-20">Say hello</button>
-    //   </div>
-    // </div>
-
-    // Filters
+    // Animations and transitions
     <div className="h-screen p-4 Parent w-64">
-      <div className="incline-block p-4 text-black bg-white border rounded-lg shadow-md grayscale brightness-50 hover:brightness-100">
-        <img src="https://www.tangledupinfood.com/sites/default/files/monaco_new_harbor.jpg" alt="" className="w-64 mb-4"/>
+      <div className="incline-block p-4 text-black bg-white border rounded-lg shadow-md">
+        <img src="https://www.tangledupinfood.com/sites/default/files/monaco_new_harbor.jpg" alt="" className="w-64 mb-4 transition origin-bottom-right hover:skew-y-12"/>
         <h1 className="text-2xl">Hello</h1>
         <p className="mb-4">This is my div</p>
-        <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 opacity-20">Say hello</button>
+        <button className="px-3 py-2 rounded-lg cursor-pointer transition hover:-translate-y-2 bg-cyan-500 shadow-md shadow-cyan-500/50">Say hello</button>
       </div>
     </div>
   );
